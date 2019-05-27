c1378e6fbe50b0e4030f108e55d18b888de7294b
Cassie <48208000+CassieBeryl@users.noreply.github.com>
2019-05-27 11:41:22 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+//affichage date et heure
+function date_heure(id)
+{
+        date = new Date;
+        annee = date.getFullYear();
+        moi = date.getMonth();
+        mois = new Array('Janvier', 'F&eacute;vrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Ao&ucirc;t', 'Septembre', 'Octobre', 'Novembre', 'D&eacute;cembre');
+        j = date.getDate();
+        jour = date.getDay();
+        jours = new Array('Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi');
+        h = date.getHours();
+        if(h<10)
+        {
+                h = "0"+h;
+        }
+        m = date.getMinutes();
+        if(m<10)
+        {
+                m = "0"+m;
+        }
+        s = date.getSeconds();
+        if(s<10)
+        {
+                s = "0"+s;
+        }
+        resultat = 'Nous sommes le '+jours[jour]+' '+j+' '+mois[moi]+' '+annee+' il est '+h+':'+m+':'+s;
+        document.getElementById(id).innerHTML = resultat;
+        setTimeout('date_heure("'+id+'");','1000');
+        return true;
+}
+//switch cSS0 /CSS1 thème espace
+function setActiveStyleSheet(title) {
+  var links = document.querySelectorAll('link[rel*="stylesheet"]');
+  var i = links.length;
+  while (links[--i]) {
+    links[i].disabled = links[i].getAttribute('title') !== title;
+  }
+}
